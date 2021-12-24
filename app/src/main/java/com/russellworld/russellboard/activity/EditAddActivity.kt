d6bcd39db4f291b319e5dc14510cacdc4769fc68
RassellWorld <miraqq55@gmail.com>
2021-12-25 02:17:07 +0300

    Добавление библиотеки PixImagePicker

--- app/src/main/java/com/russellworld/russellboard/activity/EditAddActivity.kt
+++ app/src/main/java/com/russellworld/russellboard/activity/EditAddActivity.kt
@@ -1,13 +1,18 @@
 package com.russellworld.russellboard.activity
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.fxn.pix.Pix
+import com.fxn.utility.PermUtil
 import com.russellworld.russellboard.R
 import com.russellworld.russellboard.databinding.ActivityEditAddBinding
 import com.russellworld.russellboard.dialogs.DialogSpinnerHelper
 import com.russellworld.russellboard.utilits.CityHelper
+import com.russellworld.russellboard.utilits.ImagePicker
 
 class EditAddActivity : AppCompatActivity() {
     lateinit var rootElement: ActivityEditAddBinding
@@ -21,6 +26,37 @@ class EditAddActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == ImagePicker.REQUEST_CODE_GET_IMAGES) {
+            if (data != null) {
+                val returnValue = data.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ImagePicker.getImages(this)
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Approve permissions to open Pix ImagePicker", Toast.LENGTH_LONG
+                    )
+                        .show()
+                }
+                return
+            }
+        }
+    }
+
     private fun init() {
 
     }
@@ -29,7 +65,7 @@ class EditAddActivity : AppCompatActivity() {
     fun onClickSelectCountry(view: View) {
         val listCountry = CityHelper.getAllCountries(this)
         dialog.showSpinnerDialog(this, listCountry, rootElement.tvCountry)
-        if (rootElement.tvCity.text.toString() != getString(R.string.select_city)){
+        if (rootElement.tvCity.text.toString() != getString(R.string.select_city)) {
             rootElement.tvCity.text = getString(R.string.select_city)
         }
     }
@@ -42,5 +78,9 @@ class EditAddActivity : AppCompatActivity() {
         } else {
             Toast.makeText(this, "No country selected", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun onClickGetImages(view: View) {
+        ImagePicker.getImages(this)
     }
 }
